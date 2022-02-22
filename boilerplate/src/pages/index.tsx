614d176c9ab63fadfb36712885641fa2f7e29b25
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-02-22 17:12:14 -0300

    Using context, theme and api

--- boilerplate/src/pages/index.tsx
+++ boilerplate/src/pages/index.tsx
@@ -1,3 +1,24 @@
+import { User } from 'interfaces/User/types'
+
+import useFetch from 'hooks/useFetch'
+
+import { UserContext } from 'contexts/User/UserContext'
+
+import Layout from 'components/Layout'
+
+import { ThemeProvider } from 'styled-components'
+import light from 'styles/themes/light'
+
 export default function Home() {
-	return <div></div>
+	const { data: user, error } = useFetch<User>('http://localhost:3000/api')
+
+	if (error) return <p>There is an error.</p>
+	if (!user) return <div>Loading</div>
+	return (
+		<ThemeProvider theme={light}>
+			<UserContext.Provider value={{ user }}>
+				<Layout />
+			</UserContext.Provider>
+		</ThemeProvider>
+	)
 }
