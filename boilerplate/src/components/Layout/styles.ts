148fa6470fb86cde8c8affa4bdabb83910273003
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-02-23 11:37:13 -0300

    Removed grid template

--- boilerplate/src/components/Layout/styles.ts
+++ boilerplate/src/components/Layout/styles.ts
@@ -1,9 +1,6 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-	display: grid;
-	grid-template-rows: repeat(5, auto);
-
 	p::selection {
 		color: ${(props) => props.theme.colors.white};
 		background: ${({ theme }) => theme.colors.v1};
