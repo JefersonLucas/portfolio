499059fd3d019b99c072816770db167d70190924
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-02-23 10:45:59 -0300

    Added grid rows

--- boilerplate/src/components/Layout/styles.ts
+++ boilerplate/src/components/Layout/styles.ts
@@ -1,6 +1,9 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
+	display: grid;
+	grid-template-rows: repeat(5, auto);
+
 	p::selection {
 		color: ${(props) => props.theme.colors.white};
 		background: ${({ theme }) => theme.colors.v1};
