ab54a27175bf8cab7d9cbd2f1e121242d40a1457
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-02-23 12:17:17 -0300

    Added main component

--- boilerplate/src/components/Layout/index.tsx
+++ boilerplate/src/components/Layout/index.tsx
@@ -3,11 +3,13 @@ import React from 'react'
 import { Container } from './styles'
 
 import Header from 'components/Header'
+import Main from 'components/Main'
 
 const Layout: React.FC = () => {
 	return (
 		<Container>
 			<Header />
+			<Main />
 		</Container>
 	)
 }
