23b8c1261931de68b18852b20acddb5fe390413a
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-02-24 09:44:05 -0300

    Removed styles

--- boilerplate/src/components/About/styles.ts
+++ boilerplate/src/components/About/styles.ts
@@ -1,25 +1,7 @@
 import styled from 'styled-components'
 
-export const Container = styled.section`
-	display: grid;
-	grid-template-columns: 1fr 2fr;
-	gap: 100px;
-
-	max-width: 1200px;
-	height: 100vh;
-	padding: 40px 20px;
-	margin: 0 auto;
-`
-
-export const Content = styled.div``
-
-export const Description = styled.p`
-	font-size: 1.5rem;
-	line-height: 1.33;
-	margin-bottom: 60px;
-	color: ${({ theme }) => theme.colors.g10};
-
-	> strong {
-		color: ${({ theme }) => theme.colors.black};
+export const Content = styled.div`
+	p {
+		margin-bottom: 60px;
 	}
 `
