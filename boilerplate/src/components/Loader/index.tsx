04f0f209f2ac7bef33bd4fbb059c74c2a4bda333
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-02-25 10:11:55 -0300

    Created loader component

---
+++ boilerplate/src/components/Loader/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Container, Load } from './styles'
+
+const Loader: React.FC = () => {
+	return (
+		<Container>
+			<Load />
+		</Container>
+	)
+}
+
+export default Loader
