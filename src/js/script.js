a9225d5f868ff15cfc7b22d26335716684f5a321
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-08-03 12:04:33 -0300

    Created function to animation on scroll

--- src/js/script.js
+++ src/js/script.js
@@ -2,15 +2,17 @@ window.addEventListener("load", () => {
 	document.getElementById("preload").remove();
 
 	const typewriterElement = document.querySelector('[data-type="writer"]');
+	const sectionsElements = document.querySelectorAll("section");
 
 	const stacks = ["Front End", "Back End", "Full Stack"];
+	const halfWindow = window.innerHeight * 0.5;
 
 	let messageIndex, characterIndex, currentMessage, currentCharacters;
 
 	messageIndex = characterIndex = 0;
 	currentMessage = currentCharacters = "";
 
-	const type = () => {
+	function type() {
 		const shouldTypeFirsMessage = messageIndex === stacks.length;
 
 		shouldTypeFirsMessage && (messageIndex = 0);
@@ -26,7 +28,19 @@ window.addEventListener("load", () => {
 			messageIndex++;
 			characterIndex = 0;
 		}
-	};
+	}
+
+	function animationOnScroll() {
+		sectionsElements.forEach((section) => {
+			const sectionOnTop = section.getBoundingClientRect().top;
+			const isSectionVisible = sectionOnTop - halfWindow < 0;
+			isSectionVisible
+				? section.classList.add("animation-scroll")
+				: section.classList.remove("animation-scroll");
+		});
+	}
+
+	window.addEventListener("scroll", animationOnScroll);
 
 	setInterval(type, 400);
 });
